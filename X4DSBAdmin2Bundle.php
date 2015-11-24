93a743c7b64b4f43bba9ef924a8e5e02aa546ff5
Leo van Alphen <leovanalphen@gmail.com>
2015-11-24 10:04:06 +0100

    namespaces

--- X4DSBAdmin2Bundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace X4D\SBAdmin2Bundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class X4DSBAdmin2Bundle extends Bundle
-{
-}
---
+++ iggnitionSBAdmin2Bundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace iggnition\SBAdmin2Bundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class iggnitionSBAdmin2Bundle extends Bundle
+{
+}
